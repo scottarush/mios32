3e11bce2067744d11c550384678bd776485283b3
Scott Rush <scottarush@yahoo.com>
2024-06-08 10:15:13 -0400

    initial commit of concept m3 superpedal files

---
+++ apps/controllers/m3_superpedal/setenvars.sh
@@ -0,0 +1,8 @@
+export PATH=$PATH:/home/scott/gnutools/gnu-tools-for-stm32-12.3.rel1/install-native/bin
+export MIOS32_PATH=/home/scott/midibox/mios32
+export MIOS32_BIN_PATH=$MIOS32_PATH/bin
+export MIOS32_GCC_PREFIX=arm-none-eabi
+export MIOS32_FAMILY=STM32F4xx
+export MIOS32_PROCESSOR=STM32F407VG
+export MIOS32_BOARD=MBHP_CORE_STM32F4
+export MIOS32_LCD=universal
