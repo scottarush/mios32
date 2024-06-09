d09508c97cc8137f67b4274fe04e5746fbf96d51
Scott Rush <scottarush@yahoo.com>
2024-06-09 13:17:24 -0400

    initial compileable draft

---
+++ apps/controllers/m3_superpedal/src/mios32_config.h
@@ -0,0 +1,42 @@
+// $Id$
+/*
+ * Local MIOS32 configuration file
+ *
+ * this file allows to disable (or re-configure) default functions of MIOS32
+ * available switches are listed in $MIOS32_PATH/docs/mios32/MIOS32_CONFIG.txt
+ *
+ */
+
+#ifndef _MIOS32_CONFIG_H
+#define _MIOS32_CONFIG_H
+
+// The boot message which is print during startup and returned on a SysEx query
+#define MIOS32_LCD_BOOT_MSG_LINE1 "M3 Super Pedal"
+#define MIOS32_LCD_BOOT_MSG_LINE2 "(C) 2024 S. Rush"
+
+
+// Following settings allow to customize the USB device descriptor
+#define MIOS32_USB_VENDOR_STR   "jsrglobal.net" // you will see this in the USB device description
+#define MIOS32_USB_PRODUCT_STR  "M3 SuperPedal"   // you will see this in the MIDI device list
+
+#define MIOS32_USB_MIDI_NUM_PORTS 1           // 1 MIDI Outs
+
+// Don't use IIC or SPI
+#define MIOS32_DONT_USE_IIC_MIDI
+#define MIOS32_DONT_USE_SPI_MIDI
+
+// No need for AINs
+#define MIOS32_DONT_USE_AIN
+
+// Need two UARTS:  MO1 (UART2) and MO2 (UART3)
+// TODO:  Figure out how to properly handle the UART defines
+#define MIOS32_UART_NUM 4                 
+
+// EEPROM emulation
+// SIZE == 2048 halfwords -> 4096 bytes
+#define EEPROM_EMULATED_SIZE 2048
+
+// magic number in EEPROM 'M3SP' - if it doesn't exist at address 0x00..0x03, the EEPROM will be cleared
+#define EEPROM_MAGIC_NUMBER 0x6D337370 
+
+#endif /* _MIOS32_CONFIG_H */
