15c76dddb618c9b331b9ed8d7c7cc51b9eb34b99
Scott Rush <scottarush@yahoo.com>
2024-07-14 21:00:59 -0400

    Added Disklavier voice data

--- apps/controllers/m3_superpedal/src/midi_patch_data.h
+++ apps/controllers/m3_superpedal/src/midi_patch_data.h
@@ -13,13 +13,13 @@
 // Bank select Control Change number for JV880 Internal Presets.  See JV880 Manual page 10-32
 #define JV880_INTERNAL_PRESET_BANK_SELECT_CC0 81
 
-// Structure for disklavier patches
-typedef struct disklavier_midi_patch_s {
+// Structure for disklavier patches.
+typedef struct disklavier_voice_midi_number_s {
    u8 groupID;
    u8 programNumber;
    u8 bankNumber;
-   const char * voiceName[];
-} disklavier_midi_patch_t;
+} disklavier_voice_midi_number_t;
+
 
 //////////////////////////////////////////////////////////////////////
 // Global exports
@@ -29,7 +29,12 @@ extern const char* defaultGenMIDIBankNames[NUM_GEN_MIDI_BANK_NAMES];
 #define NUM_GEN_MIDI_VOICE_NAMES 128
 extern const char* genMIDIVoiceNames[NUM_GEN_MIDI_VOICE_NAMES];
 
-#define NUM_DISKLAVIER_PATCHES 438
-extern const disklavier_midi_patch_t disklavierPatches[NUM_DISKLAVIER_PATCHES];
+#define NUM_DISKLAVIER_VOICES 438
+extern const disklavier_voice_midi_number_t disklavierVoiceMIDINumbers[NUM_DISKLAVIER_VOICES];
+extern const char* disklavierVoiceNames[NUM_DISKLAVIER_VOICES];
+
+#define NUM_DISKLAVIER_GROUPS 16
+extern const char * disklavierGroupNames[NUM_DISKLAVIER_GROUPS];
+extern const int disklavierGroupIndex[NUM_DISKLAVIER_GROUPS];
 
 #endif /* _MIDI_PATCH_DATA_H */
