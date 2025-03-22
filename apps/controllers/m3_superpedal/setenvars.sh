d95349cfc0461bdf3e0190d68a6b93dd4ebbfc02
Scott Rush <scottarush@yahoo.com>
2025-03-22 12:08:55 -0400

    updated paths to move gnutools and mios32 toolchain into /usr/lib

--- apps/controllers/m3_superpedal/setenvars.sh
+++ apps/controllers/m3_superpedal/setenvars.sh
@@ -1,6 +1,6 @@
-export PATH=$PATH:/home/scott/gnutools/gnu-tools-for-stm32-12.3.rel1/install-native/bin
-export MIOS32_PATH=/home/scott/midibox/mios32
-export MIOS32_BIN_PATH=$MIOS32_PATH/bin
+export PATH=$PATH:/usr/lib/gnutools/gnu-tools-for-stm32-12.3.rel1/install-native/bin
+export MIOS32_PATH=~/midibox/mios32
+export MIOS32_BIN_PATH=/usr/lib/mios32_toochain/bin
 export MIOS32_GCC_PREFIX=arm-none-eabi
 export MIOS32_FAMILY=STM32F4xx
 export MIOS32_PROCESSOR=STM32F407VG
