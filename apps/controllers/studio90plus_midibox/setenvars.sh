dc3a9e6c34c689f0b66eefe86b18787afb9feb38
Scott Rush <scottarush@yahoo.com>
2025-04-21 20:31:16 -0400

    Updated paths for new ubuntu-pi install

--- apps/controllers/studio90plus_midibox/setenvars.sh
+++ apps/controllers/studio90plus_midibox/setenvars.sh
@@ -1,6 +1,6 @@
-export PATH=$PATH:/usr/lib/gnutools/gnu-tools-for-stm32-12.3.rel1/install-native/bin
-export MIOS32_PATH=~/midibox/mios32
-export MIOS32_BIN_PATH=/usr/lib/mios32_toochain/bin
+export PATH=$PATH:/usr/lib/gnu-tools-for-stm32-12.3.rel1/bin
+export MIOS32_PATH=~/projects/mios32
+export MIOS32_BIN_PATH=/usr/lib/mios32_toolchain/bin
 export MIOS32_GCC_PREFIX=arm-none-eabi
 export MIOS32_FAMILY=STM32F4xx
 export MIOS32_PROCESSOR=STM32F407VG
